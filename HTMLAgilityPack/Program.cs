42c9ff5907e853ddd5c0116a9976e154d414fd1d
Oli Pyke <opyke@hotmail.com>
2017-03-04 13:50:48 +0000

    First parsing working

--- HTMLAgilityPack/Program.cs
+++ HTMLAgilityPack/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
 
 namespace HTMLAgilityPack
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            ScrapingBrowser browser = new ScrapingBrowser { AllowAutoRedirect = true, AllowMetaRedirect = true };
+            WebPage pageResult = browser.NavigateToPage(new Uri("https://www.bing.com/search?q=Oli"));
+            IEnumerable<HtmlNode> results = pageResult.Html.CssSelect(".b_algo");
+
+            foreach (HtmlNode node in results)
+            {
+                // var address = node.SelectSingleNode("h2/a/@href");
+                var link = node.SelectSingleNode("h2/a");
+                var address = link.GetAttributeValue("href");
+                var text = link.InnerText;
+                Console.WriteLine($"Address: {address} Text: {text}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
